159c2d6370183d261e3727a57a720ffbbca99b29
mohammed mustafa <hamato122@yahoo.com>
2026-08-31 23:30:17 +0300

    fix: next-auth type declarations for tenantId + role

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,18 +1,29 @@
-import { DefaultSession } from "next-auth";
+import "next-auth";
 
 declare module "next-auth" {
-  interface User {
-    role?: string;
-  }
   interface Session {
     user: {
-      role?: string;
-    } & DefaultSession["user"];
+      id: string;
+      name: string;
+      email: string;
+      image?: string;
+      role: string;
+      tenantId: string;
+      tenantName: string;
+    };
+  }
+
+  interface User {
+    role: string;
+    tenantId: string;
+    tenantName: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     role?: string;
+    tenantId?: string;
+    tenantName?: string;
   }
 }
